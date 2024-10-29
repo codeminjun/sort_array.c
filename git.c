e7509ccc2662ef182be31e946640b43ea8c1afdf
김민준 <koexmin@gmail.com>
2024-10-29 20:44:15 +0900

    strtok 루프제작

--- git.c
+++ git.c
@@ -5,11 +5,20 @@
 
 int main(void)
 {
-    int i,j,len;
+    int j,len;
+    int i=0;
     char num[100];
+    char *new[100];
     char *token;
 
     printf("문자열을 입력하세요: ");
     fgets(num,sizeof(num),stdin);
     num[strcspn(num,"\n")] = '\0';
+
+    token = strtok(num," ");
+    while(token != NULL){
+        new[i] = token;
+        i++;
+        token = strtok(NULL," ");
+    }
 }
